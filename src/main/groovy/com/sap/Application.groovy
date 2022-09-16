f5fde786213659e19a693f7140639b97c23642bf
DeepankarBhowmick <deepankar.bhowmick@gmail.com>
2022-09-16 15:30:56 +0200

    Changed the initialization.

--- src/main/groovy/com/sap/Application.groovy
+++ src/main/groovy/com/sap/Application.groovy
@@ -11,9 +11,10 @@ class Application {
     static void main(String[] args) {
 //        Micronaut.run(Application, args)
         String userName, passWord, url
-        CfService cfService = new CfEnv().findServiceByLabel('hana')
+        CfService cfService
         Micronaut micronaut = Micronaut.build(args)
-        if (cfService != null) {
+        try {
+            cfService = new CfEnv().findServiceByLabel('hana')
             passWord = cfService.getCredentials().getPassword()
             userName = cfService.getCredentials().getUsername()
             url = cfService.getCredentials().getMap().get('url')
@@ -22,7 +23,8 @@ class Application {
                                                          'datasources.default.username'       : userName,
                                                          'datasources.default.password'       : passWord,
                                                          'datasources.default.schema-generate': 'CREATE_DROP'] as Map<String, Object>)).start()
-        } else {
+        }
+        catch (Exception exception) {
             micronaut.propertySources(PropertySource.of(['datasources.default.url'            : 'jdbc:h2:mem:devDb;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE',
                                                          'datasources.default.driverClassName': 'org.h2.Driver',
                                                          'datasources.default.username'       : 'sa',
@@ -30,5 +32,22 @@ class Application {
                                                          'datasources.default.schema-generate': 'CREATE_DROP',
                                                          'datasources.default.dialect'        : 'H2'] as Map<String, Object>)).start()
         }
+//        if (cfService != null) {
+//            passWord = cfService.getCredentials().getPassword()
+//            userName = cfService.getCredentials().getUsername()
+//            url = cfService.getCredentials().getMap().get('url')
+//            micronaut.propertySources(PropertySource.of(['datasources.default.url'            : url,
+//                                                         'datasources.default.driverClassName': 'com.sap.db.jdbc.Driver',
+//                                                         'datasources.default.username'       : userName,
+//                                                         'datasources.default.password'       : passWord,
+//                                                         'datasources.default.schema-generate': 'CREATE_DROP'] as Map<String, Object>)).start()
+//        } else {
+//            micronaut.propertySources(PropertySource.of(['datasources.default.url'            : 'jdbc:h2:mem:devDb;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE',
+//                                                         'datasources.default.driverClassName': 'org.h2.Driver',
+//                                                         'datasources.default.username'       : 'sa',
+//                                                         'datasources.default.password'       : '',
+//                                                         'datasources.default.schema-generate': 'CREATE_DROP',
+//                                                         'datasources.default.dialect'        : 'H2'] as Map<String, Object>)).start()
+//        }
     }
 }
