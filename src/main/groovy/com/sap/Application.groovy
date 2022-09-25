57574bf3d03a0bc4dc3dac039a2180e79a23834c
DeepankarBhowmick <deepankar.bhowmick@gmail.com>
2022-09-25 11:21:27 +0200

    Changed the initialization.

--- src/main/groovy/com/sap/Application.groovy
+++ src/main/groovy/com/sap/Application.groovy
@@ -32,22 +32,5 @@ class Application {
                                                          'datasources.default.schema-generate': 'CREATE_DROP',
                                                          'datasources.default.dialect'        : 'H2'] as Map<String, Object>)).start()
         }
-//        if (cfService != null) {
-//            passWord = cfService.getCredentials().getPassword()
-//            userName = cfService.getCredentials().getUsername()
-//            url = cfService.getCredentials().getMap().get('url')
-//            micronaut.propertySources(PropertySource.of(['datasources.default.url'            : url,
-//                                                         'datasources.default.driverClassName': 'com.sap.db.jdbc.Driver',
-//                                                         'datasources.default.username'       : userName,
-//                                                         'datasources.default.password'       : passWord,
-//                                                         'datasources.default.schema-generate': 'CREATE_DROP'] as Map<String, Object>)).start()
-//        } else {
-//            micronaut.propertySources(PropertySource.of(['datasources.default.url'            : 'jdbc:h2:mem:devDb;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE',
-//                                                         'datasources.default.driverClassName': 'org.h2.Driver',
-//                                                         'datasources.default.username'       : 'sa',
-//                                                         'datasources.default.password'       : '',
-//                                                         'datasources.default.schema-generate': 'CREATE_DROP',
-//                                                         'datasources.default.dialect'        : 'H2'] as Map<String, Object>)).start()
-//        }
     }
 }
